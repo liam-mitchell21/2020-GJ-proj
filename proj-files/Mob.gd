9cf2b6d4f0437b3f2d8b222dc7b2289b26bff739
Liam R Mitchell <liam.mitchell@uleth.ca>
2020-02-17 23:11:20 -0700

    fixed issue to finish up 'dodge the creeps' demo

--- proj-files/Mob.gd
+++ proj-files/Mob.gd
@@ -11,7 +11,7 @@ var mob_types = ["walk", "swim", "fly"]
 func _ready():
 	$AnimatedSprite.animation = mob_types[randi() % mob_types.size()]
 
-func _on_Visibility_screen_exited():
+func _on_VisibilityNotifier2D_screen_exited():
 	queue_free()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
--- proj-files/Player.gd
+++ proj-files/Player.gd
@@ -2,21 +2,12 @@ extends Area2D
 
 signal hit
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
 export var speed = 400 #(px per sec)
 var screen_size
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	screen_size = get_viewport_rect().size
 	hide()
-
-func start(pos):
-	position = pos
-	show()
-	$CollisionShape2D.disabled = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -49,7 +40,13 @@ func _process(delta):
 		$AnimatedSprite.flip_v = velocity.y > 0
 
 
-func _on_Player_body_entered(body):
+func start(pos):
+	position = pos
+	show()
+	$CollisionShape2D.disabled = false
+
+func _on_Player_body_entered(_body):
 	hide()
 	emit_signal("hit")
 	$CollisionShape2D.set_deferred("disabled", true)
+
